eba2943f08f37b3b4b4033686c47c311aff44bc8
Your Name <you@example.com>
2019-07-24 16:26:06 -0500

    version 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuPrincipalComponent } from './componentes/menu-principal/menu-principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuPrincipalComponent,
+    MenuPrincipalComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/consultar-historial/consultarHistorial.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'consultarHistorial',
+    templateUrl: './consultarHistorial.component.html',
+    styleUrls: ['./consultarHistorial.component.css']
+})
+export class ConsultarHistorial{
+
+}
